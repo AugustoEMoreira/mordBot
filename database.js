ccba8e70430f1873c87564244ede4a605268e41a
Augusto Evangelista Moreira <augusto.moreira@mercadolivre.com>
2021-04-30 22:28:44 -0300

    adding leadboard

--- database.js
+++ database.js
@@ -1,5 +1,8 @@
+const axios = require('axios')
+
 const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(':memory')
+
 
 function addKill(playfabIdKiller, playfabIdKilled) {
     return new Promise(resolve => {
@@ -11,7 +14,7 @@ function addKill(playfabIdKiller, playfabIdKilled) {
             createPlayers.push(createPlayer(playfabIdKilled))
 
             Promise.all(createPlayers).then(_ => {
-                db.run('insert into match (winnerId,loserId) values (?,?)',[playfabIdKiller,playfabIdKilled])
+                db.run('insert into match (winnerId,loserId) values (?,?)', [playfabIdKiller, playfabIdKilled])
                 db.run('UPDATE players set kills = IFNULL(kills, 0) + 1 where playfabid = ?', [playfabIdKiller], (err) => {
                     if (err) throw new Error;
                     db.run('UPDATE players set deaths = IFNULL(deaths, 0) + 1 where playfabid = ?', [playfabIdKilled], (err2) => {
@@ -31,15 +34,92 @@ function createPlayer(playfabId) {
         })
     })
 }
-function getKda(playfabId){
-    return new Promise(resolve=>{
-        db.get('SELECT * from players where playfabId = ?',[playfabId],(err,row)=>{
-            if(err) throw new Error(err)
+function getKda(playfabId) {
+    return new Promise(resolve => {
+        db.get('SELECT * from players where playfabId = ?', [playfabId], (err, row) => {
+            if (err) throw new Error(err)
             resolve(row)
         })
     })
 }
+function getTop10() {
+    return new Promise(resolve => {
+        db.all('SELECT playfabId,(kills/deaths) as ratio from players order by (kills/deaths) desc limit 10', [], (err, row) => {
+            if (err) throw new Error(err)
+            let nicks = []
+            row.forEach(r => {
+                nicks.push(getNick(r.playfabId))
+            })
+            Promise.all(nicks).then(values =>{
+                let arr = values.map((item,i)=> Object.assign({},item,row[i]))
+                console.log(arr)
+                resolve(arr)
+
+            })
+        })
+    })
+}
+function getGameId(playfabId) {
+    return new Promise(resolve => {
+        const options = {
+            method: 'POST',
+            url: 'https://12d56.playfabapi.com/Client/GetPlayerCombinedInfo',
+            params: { sdk: 'UE4MKPL-1.21.190717' },
+            headers: {
+                'Content-Type': 'application/json',
+                'x-authorization': '76FDA7D9281A2C5D--E524B3C4EA184B83-12D56-8D90B82EF1926DE-aqOvnj3dnueD4IDkIPkmkEu+g8XN3UVVh87HyMTwh8g='
+            },
+            data: {
+                InfoRequestParameters: {
+                    GetCharacterInventories: false,
+                    GetCharacterList: false,
+                    GetPlayerProfile: false,
+                    GetPlayerStatistics: false,
+                    GetTitleData: false,
+                    GetUserAccountInfo: true,
+                    GetUserData: false,
+                    GetUserInventory: false,
+                    GetUserReadOnlyData: false,
+                    GetUserVirtualCurrency: false
+                },
+                PlayFabId: playfabId
+            }
+        };
+
+        axios.request(options).then(function (response) {
+            resolve(response.data.data.InfoResultPayload.AccountInfo.TitleInfo.TitlePlayerAccount.Id);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    })
+
+}
+function getNick(playfabId) {
+    return new Promise(resolve => {
+        getGameId(playfabId).then(gameId =>{
+            const options = {
+                method: 'POST',
+                url: 'https://12d56.playfabapi.com/Object/GetObjects',
+                headers: {
+                    'X-EntityToken': 'M3x7ImkiOiIyMDIxLTA0LTMwVDAzOjUwOjQ0LjMxMjQ2NzFaIiwiaWRwIjoiQ3VzdG9tIiwiZSI6IjIwMjEtMDUtMDFUMDM6NTA6NDQuMzEyNDY3MVoiLCJoIjoiRTlBRkI0Q0IzQTI4QzgyQiIsInMiOiJ6U2poOVNoOUdIcVl4NUNjOVBMUWthOHRJSkV2eHBmNDBGZWF3Q2JoS0xnPSIsImVjIjoidGl0bGVfcGxheWVyX2FjY291bnQhNDAzNkVENzE5ODM4MjY4MC8xMkQ1Ni83NkZEQTdEOTI4MUEyQzVELzM0QUU2N0VBRDlDRTcxNTIvIiwiZWkiOiIzNEFFNjdFQUQ5Q0U3MTUyIiwiZXQiOiJ0aXRsZV9wbGF5ZXJfYWNjb3VudCJ9',
+                    'Content-Type': 'application/json'
+                },
+                data: { Entity: { Id: gameId, Type: 'title_player_account' } }
+            };
+            axios.request(options).then(response => {
+                let result = {};
+                result.nick = response.data.data.Objects.AccountInfo.DataObject.Name
+                result.playfabId = response.data.data.Objects.AccountInfo.DataObject.PlayFabId
+                resolve(result)
+            }).catch(e => { throw new Error(e) })
+        })
+        
+    })
+
+}
+
 module.exports = {
     addKill,
-    getKda
+    getKda,
+    getTop10
 }
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const config = require('./config')
 const client = new Discord.Client();
 
 let conn = new Rcon(config.server.ip, config.server.port, config.server.password);
-
+let lastTopUsage
+let top
 conn.on('auth', () => {
     console.log('connectado')
     conn.send('listen allon')
@@ -17,65 +18,75 @@ conn.on('auth', () => {
 }).on('server', str => {
     if (str.startsWith('Killfeed')) {
         arr = str.match(/([0-9A-F]{14,16})/gm)
-        console.log(arr)
         database.addKill(arr[0], arr[1])
     }
     if (str.startsWith('Chat')) {
         let player = str.split(',')[0].replace('Chat: ', '')
         let playername = str.split(',')[1]
-        let chat = str.replace(/^([^,]*,[^,]*),/, '').replace(/\([^()]*\)/g, '').replace(/ /g, '')
+        let chat = str.replace(/^([^,]*,[^,]*),/, '').replace(/\([^()]*\)/g, '').trimStart()//remove names and IDs and whitespaces before the real string
+
         if (chat.startsWith('!')) {
             if (chat == '!help') {
-                conn.send('say ****** burguesinho bot de tests ******* \n !help - show all commands \n !kda - show your K-D \n !adm "describe and inform the player causing problems" - the missusege of this command can lead to ban')
-            } else {
-                if (chat == "!kda") {
-                    database.getKda(player).then(p => {
-                        if (p == undefined) {
-                            conn.send(`say ${playername} can't find you :/`)
-                        } else {
-                            conn.send(`say ${playername} - ${(p.kills == null) ? 0 : p.kills} kills - ${(p.deaths == null) ? 0 : p.deaths} deaths`)
-                        }
-                    }).catch(e => { throw new Error(e) })
-                } if (chat.startsWith("!adm")) {
-                    client.channels.fetch(config.discord.warnModeratorsChannelId).then(channel=>{
-                        channel.send(`<@&${config.discord.moderatorsRoleId}> ${str.split(',')[1]} - ${player} - help wanted: ${chat}`)
-                    })
-                }
+                conn.send('say !help - show all commands \n !kda - show your K-D \n !top - List the top 10 player and their ratio - !adm "describe and inform the player causing problems" - the missusege of this command can lead to ban')
+            }
+            if (chat == "!kda") {
+                database.getKda(player).then(p => {
+                    if (p == undefined) {
+                        conn.send(`say ${playername} can't find you :/`)
+                    } else {
+                        conn.send(`say ${playername} - ${(p.kills == null) ? 0 : p.kills} kills - ${(p.deaths == null) ? 0 : p.deaths} deaths`)
+                    }
+                }).catch(e => { throw new Error(e) })
+            } if (chat.startsWith("!adm")) {
+                client.channels.fetch(config.discord.warnModeratorsChannelId).then(channel => {
+                    channel.send(`<@&${config.discord.moderatorsRoleId}> ${str.split(',')[1]} - ${player} - help wanted: ${chat}`)
+                })
+            } if (chat.startsWith('!top')) {
+                database.getTop10().then(row => {
+                    if (top != undefined || (new Date() - lastTopUsage) < 15) {
+                        conn.send(top)
+                    } else {
+                        let response = 'say ****** Top Players ******\n'
+                        row.forEach(r => {
+                            response = response + `${r.nick} - KD Ratio - ${r.ratio}\n`
+                        })
+                        conn.send(response)
+                        top = response
+                        lastTopUsage = new Date()
+                    }
+                })
             }
         }
     }
-    if(str.startsWith('Punishment')){
-        if(str.includes(' banned')){
-                if(!str.includes('Temporarily banned')&&!str.includes('Kicked')){
-                let punishment = str.replace('Punishment: ','')
-                console.log(punishment)
-                client.channels.fetch(config.discord.banlistchannelId).then(channel=>{
+    if (str.startsWith('Punishment')) {
+        if (str.includes(' banned')) {
+            if (!str.includes('Temporarily banned') && !str.includes('Kicked')) {
+                let punishment = str.replace('Punishment: ', '')
+                client.channels.fetch(config.discord.banlistchannelId).then(channel => {
                     channel.send(punishment)
                 })
             }
         }
     }
-    else{
-        console.log(str)
-    }
 }).on('end', str => {
-    console.log('end')
+    console.log('reconnecting...')
+    setTimeout(() => {
+        conn.connect()
+    }, 10000)
 }).on('error', err => {
     console.log(err)
 })
 
-client.on('message',message =>{
-    if(message.author.bot) return
-    if(!message.content.startsWith(';')) return
-    if(message.content.startsWith(';ban') && message.content.replace(";ban ",'').split(/(?<=^[^,]+(?:,[^,]+)?), /).length == 3){
-        
-        let player = message.content.replace(";ban ",'').split(/(?<=^[^,]+(?:,[^,]+)?), /)[0]
-        let duration = message.content.replace(";ban ",'').split(/(?<=^[^,]+(?:,[^,]+)?), /)[1]
-        let reason = message.content.replace(";ban ",'').split(/(?<=^[^,]+(?:,[^,]+)?), /)[2]
+client.on('message', message => {
+    if (message.author.bot) return
+    if (!message.content.startsWith(';')) return
+    if (message.content.startsWith(';ban') && message.content.replace(";ban ", '').split(/(?<=^[^,]+(?:,[^,]+)?), /).length == 3) {
+
+        let player = message.content.replace(";ban ", '').split(/(?<=^[^,]+(?:,[^,]+)?), /)[0]
+        let duration = message.content.replace(";ban ", '').split(/(?<=^[^,]+(?:,[^,]+)?), /)[1]
+        let reason = message.content.replace(";ban ", '').split(/(?<=^[^,]+(?:,[^,]+)?), /)[2]
         conn.send(`ban ${player} ${duration} ${reason}`)
         client.channels.cache.get(config.discord.banlistchannelId).send(`Discord admin ${message.author.username} banned player ${player} (Duration: ${duration}, Reason: ${reason})`)
-    }else{
-        console.log(message.content.replace(";ban ",'').split(/(?<=^[^ ]+(?: [^ ]+)?), /))
     }
 })
 
